4a9efd1fdb6f93ba78a6db98d277984b0c2ef817
DDegre <ddegre@bancroftschool.org>
2018-05-14 09:47:54 -0400

    Add files via upload

--- sketch_apr26a.ino
+++ sketch_apr26a.ino
@@ -1,15 +1,59 @@
-const int rs = 5, en = 7, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
+int buttonA;
+
+int buttonB;
+
+const int rs = 6, en = 7, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal  lcd(rs, en, d4, d5, d6, d7);
 
-void setup () {
+void setup() {
 
+  pinMode(3, INPUT);
+  pinMode(5, INPUT);
+  Serial.begin(9600);
+  lcd.setCursor(0,1);
   lcd.begin(16, 2);
-  lcd.print("Hi Meow Im A Cow");
+
 }
 
-void loop () {
+void loop() {
+  lcd.print("Would You Rather");
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+  lcd.print("Eat Chez");
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+  lcd.print("Or Cheese");
+  delay(1000);
+  lcd.clear();
 
-lcd.setCursor(0, 1);
 
+lcd.print("Please Press a Button");
+
+while(buttonA == 0 && buttonB == 0) { 
+  Serial.println(buttonB);
+  Serial.println(buttonA);
+  buttonA = digitalRead(3);
+  buttonB = digitalRead(5);
+  delay(5);
+}
+
+if (buttonA == 1);{
+
+lcd.clear();
+lcd.print("Chez");
+delay(5000);
+
+}
+
+else (buttonB == 1);{
+
+lcd.clear();
+lcd.print("Cheese");
+delay(5000);
+
+}
+}
 }
