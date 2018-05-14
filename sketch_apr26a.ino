455d27883504fc3ba0e53805ade6bdbbd0996067
DDegre <ddegre@bancroftschool.org>
2018-05-14 10:46:01 -0400

    Add files via upload

--- sketch_apr26a.ino
+++ sketch_apr26a.ino
@@ -17,15 +17,21 @@ void setup() {
 }
 
 void loop() {
-  lcd.print("Would You Rather");
+  spower();
+   slime();
+}
+
+void spower(){
+
+lcd.print("Would You Rather");
   delay(1000);
   lcd.clear();
   delay(1000);
-  lcd.print("Eat Chez");
+  lcd.print("Teleport Anywhere");
   delay(1000);
-  lcd.clear();
+ lcd.clear();
   delay(1000);
-  lcd.print("Or Cheese");
+  lcd.print("Or Read Minds");
   delay(1000);
   lcd.clear();
 
@@ -40,20 +46,66 @@ while(buttonA == 0 && buttonB == 0) {
   delay(5);
 }
 
-if (buttonA == 1);{
+if (buttonA == 1){
 
 lcd.clear();
-lcd.print("Chez");
+lcd.print("SEE You Soon!!!");
 delay(5000);
-
+lcd.clear();
 }
 
-else (buttonB == 1);{
+else {
 
 lcd.clear();
-lcd.print("Cheese");
+lcd.print("What Am I THINKING!!!");
 delay(5000);
+lcd.clear();
 
 }
+lcd.clear();
 }
+
+void slime(){
+
+lcd.print("Would You Rather");
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+  lcd.print("Have Slime Hands");
+  delay(1000);
+ lcd.clear();
+  delay(1000);
+  lcd.print("Or Slime Feet");
+  delay(1000);
+  lcd.clear();
+
+
+lcd.print("Please Press a Button");
+
+while(buttonA == 0 && buttonB == 0) { 
+  Serial.println(buttonB);
+  Serial.println(buttonA);
+  buttonA = digitalRead(3);
+  buttonB = digitalRead(5);
+  delay(5);
+}
+
+if (buttonA == 1){
+
+lcd.clear();
+lcd.print("Need A HAND!!!");
+delay(5000);
+lcd.clear();
+}
+
+else {
+
+lcd.clear();
+
+lcd.print("The Floors SLIPPERY!!!");
+delay(5000);
+lcd.clear();
+
+}
+lcd.clear();
 }
