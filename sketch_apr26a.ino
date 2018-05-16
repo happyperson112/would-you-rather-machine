720cdad68315862b33b3b1f4bbf48957497c60a9
DDegre <ddegre@bancroftschool.org>
2018-05-16 09:20:44 -0400

    Add files via upload

--- sketch_apr26a.ino
+++ sketch_apr26a.ino
@@ -17,26 +17,35 @@ void setup() {
 }
 
 void loop() {
-  spower();
-   slime();
+//spower();
+//slime();
+//work();
+//timeTravel();
+soda();
+
 }
 
 void spower(){
-
-lcd.print("Would You Rather");
-  delay(1000);
+lcd.print("   Would You");
+lcd.setCursor(3, 2);
+lcd.print("  Rather");
+  delay(5000);
   lcd.clear();
   delay(1000);
-  lcd.print("Teleport Anywhere");
-  delay(1000);
+  lcd.print("  Teleport");
+  lcd.setCursor(1, 2);
+  lcd.print("  Anywhere");
+  delay(5000);
  lcd.clear();
   delay(1000);
   lcd.print("Or Read Minds");
-  delay(1000);
+  delay(5000);
   lcd.clear();
 
-
-lcd.print("Please Press a Button");
+lcd.print("Please Press a");
+lcd.setCursor(3, 2);
+lcd.print(" Button");
+lcd.setCursor(0, 1);
 
 while(buttonA == 0 && buttonB == 0) { 
   Serial.println(buttonB);
@@ -49,16 +58,18 @@ while(buttonA == 0 && buttonB == 0) {
 if (buttonA == 1){
 
 lcd.clear();
-lcd.print("SEE You Soon!!!");
-delay(5000);
+lcd.print("  WHERE am I!!!");
+delay(8000);
 lcd.clear();
 }
 
 else {
 
 lcd.clear();
-lcd.print("What Am I THINKING!!!");
-delay(5000);
+lcd.print("  What Am I ");
+lcd.setCursor(1, 2);
+lcd.print("  THINKING!!!");
+delay(8000);
 lcd.clear();
 
 }
@@ -67,20 +78,24 @@ lcd.clear();
 
 void slime(){
 
-lcd.print("Would You Rather");
-  delay(1000);
+lcd.print("   Would You");
+lcd.setCursor(3, 2);
+lcd.print("  Rather");
+  delay(5000);
   lcd.clear();
   delay(1000);
   lcd.print("Have Slime Hands");
-  delay(1000);
+  delay(5000);
  lcd.clear();
   delay(1000);
   lcd.print("Or Slime Feet");
-  delay(1000);
+  delay(5000);
   lcd.clear();
-
-
-lcd.print("Please Press a Button");
+  
+lcd.print("Please Press a");
+lcd.setCursor(3, 2);
+lcd.print(" Button");
+lcd.setCursor(0, 1);
 
 while(buttonA == 0 && buttonB == 0) { 
   Serial.println(buttonB);
@@ -93,17 +108,184 @@ while(buttonA == 0 && buttonB == 0) {
 if (buttonA == 1){
 
 lcd.clear();
-lcd.print("Need A HAND!!!");
-delay(5000);
+lcd.print("  Need A HAND!!!");
+delay(8000);
 lcd.clear();
 }
 
 else {
 
 lcd.clear();
+lcd.print("  The Floor is");
+lcd.setCursor(1, 2);
+lcd.print("  SLIPPERY!!!");
+delay(8000);
+lcd.clear();
 
-lcd.print("The Floors SLIPPERY!!!");
-delay(5000);
+}
+lcd.clear();
+}
+
+void work(){
+  
+lcd.print("   Would You");
+lcd.setCursor(3, 2);
+lcd.print("  Rather");
+  delay(5000);
+  lcd.clear();
+  delay(1000);
+  lcd.print("  Work All");
+  lcd.setCursor(1, 2);
+  lcd.print("   Day");
+  delay(5000);
+ lcd.clear();
+  delay(1000);
+  lcd.print("Or Work All");
+  lcd.setCursor(1, 2);
+  lcd.print("   Night");
+  delay(5000);
+  lcd.clear();
+
+lcd.print("Please Press a");
+lcd.setCursor(3, 2);
+lcd.print(" Button");
+lcd.setCursor(0, 1);
+
+while(buttonA == 0 && buttonB == 0) { 
+  Serial.println(buttonB);
+  Serial.println(buttonA);
+  buttonA = digitalRead(3);
+  buttonB = digitalRead(5);
+  delay(5);
+}
+
+if (buttonA == 1){
+
+lcd.clear();
+lcd.print(" What About The");
+lcd.setCursor(1, 2);
+lcd.print(" KIDS!!!");
+delay(8000);
+lcd.clear();
+}
+
+else {
+
+lcd.clear();
+lcd.print(" Bet You Are ");
+lcd.setCursor(1, 2);
+lcd.print("  TIRED!!!");
+delay(8000);
+lcd.clear();
+
+}
+lcd.clear();
+}
+
+void timeTravel(){
+  
+lcd.print("   Would You");
+lcd.setCursor(3, 2);
+lcd.print("  Rather");
+  delay(5000);
+  lcd.clear();
+  delay(1000);
+  lcd.print(" Go Into The");
+  lcd.setCursor(1, 2);
+  lcd.print("  FUTURE");
+  delay(5000);
+ lcd.clear();
+  delay(1000);
+  lcd.print("Or Go Into The");
+  lcd.setCursor(1, 2);
+  lcd.print("  PAST");
+  delay(5000);
+  lcd.clear();
+
+lcd.print("Please Press a");
+lcd.setCursor(3, 2);
+lcd.print(" Button");
+lcd.setCursor(0, 1);
+
+while(buttonA == 0 && buttonB == 0) { 
+  Serial.println(buttonB);
+  Serial.println(buttonA);
+  buttonA = digitalRead(3);
+  buttonB = digitalRead(5);
+  delay(5);
+}
+
+if (buttonA == 1){
+
+lcd.clear();
+lcd.print(" Are THERE still");
+lcd.setCursor(1, 2);
+lcd.print(" Schools!?!");
+delay(8000);
+lcd.clear();
+}
+
+else {
+
+lcd.clear();
+lcd.print(" GREAT SCOTT!!!");
+delay(8000);
+lcd.clear();
+
+}
+lcd.clear();
+}
+
+void soda(){
+  
+lcd.print("   Would You");
+lcd.setCursor(3, 2);
+lcd.print("  Rather");
+  delay(5000);
+  lcd.clear();
+  delay(1000);
+  lcd.print(" Only Drink");
+  lcd.setCursor(1, 2);
+  lcd.print("  Soda");
+  delay(5000);
+ lcd.clear();
+  delay(1000);
+  lcd.print("Or Never");
+  lcd.setCursor(1, 2);
+  lcd.print("  Drink Soda");
+  delay(5000);
+  lcd.clear();
+
+lcd.print("Please Press a");
+lcd.setCursor(3, 2);
+lcd.print(" Button");
+lcd.setCursor(0, 1);
+
+while(buttonA == 0 && buttonB == 0) { 
+  Serial.println(buttonB);
+  Serial.println(buttonA);
+  buttonA = digitalRead(3);
+  buttonB = digitalRead(5);
+  delay(5);
+}
+
+if (buttonA == 1){
+
+lcd.clear();
+lcd.print(" That Is Too ");
+lcd.setCursor(1, 2);
+lcd.print(" Much SUGAR!!!");
+delay(8000);
+lcd.clear();
+}
+
+else {
+
+lcd.clear();
+lcd.print(" You Just Made");
+lcd.setCursor(1, 2);
+lcd.print("A Good CHOICE");
+delay(8000);
 lcd.clear();
 
 }
