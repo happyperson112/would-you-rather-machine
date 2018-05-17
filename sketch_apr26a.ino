4d47fe918dc83bdde4317f0bd8d1a5a631da317d
DDegre <ddegre@bancroftschool.org>
2018-05-17 14:15:08 -0400

    Add files via upload

--- sketch_apr26a.ino
+++ sketch_apr26a.ino
@@ -21,7 +21,8 @@ void loop() {
 //slime();
 //work();
 //timeTravel();
-soda();
+//soda();
+control();
 
 }
 
@@ -291,3 +292,54 @@ lcd.clear();
 }
 lcd.clear();
 }
+
+void control(){
+  
+lcd.print("   Would You");
+lcd.setCursor(3, 2);
+lcd.print("  Rather");
+  delay(5000);
+  lcd.clear();
+  delay(1000);
+  lcd.print(" Control Space");
+  delay(5000);
+ lcd.clear();
+  delay(1000);
+  lcd.print(" Or Time");
+  delay(5000);
+  lcd.clear();
+
+lcd.print("Please Press a");
+lcd.setCursor(3, 2);
+lcd.print(" Button");
+lcd.setCursor(0, 1);
+
+while(buttonA == 0 && buttonB == 0) { 
+  Serial.println(buttonB);
+  Serial.println(buttonA);
+  buttonA = digitalRead(3);
+  buttonB = digitalRead(5);
+  delay(5);
+}
+
+if (buttonA == 1){
+
+lcd.clear();
+lcd.print("  !!!");
+delay(8000);
+lcd.clear();
+}
+
+else {
+
+lcd.clear();
+lcd.print("Wish We Could Tu");
+lcd.setCursor(1, 2);
+lcd.print("rn Back TIME!!!");
+delay(8000);
+lcd.clear();
+
+}
+lcd.clear();
+}
+
